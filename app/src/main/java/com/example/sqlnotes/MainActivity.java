5788541e0f1776b7703ac0c98f7ed7dd55f0f321
Harris B <harris@harrisbeg.com>
2019-05-21 13:46:40 -0700

    Basic app

---
+++ app/src/main/java/com/example/sqlnotes/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sqlnotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Hi", "Hello World");
+        final TextView name = (TextView) findViewById(R.id.name);
+        final TextView phone = (TextView) findViewById(R.id.phone);
+        final TextView address = (TextView) findViewById(R.id.address);
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retrieveInfo(name.getText().toString(), phone.getText().toString(), address.getText().toString());
+            }
+        });
+    }
+    public void retrieveInfo(String name, String phone, String address) {
+        DatabaseReference data = FirebaseDatabase.getInstance().getReference();
+        data.child("users").child("name").setValue("phone",phone);
+    }
+}
